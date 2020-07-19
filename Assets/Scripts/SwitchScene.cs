cd7d40413c1c51e1b3f61c548915c1bcd3ddc2fe
kdraslan <kdr.aslan@yahoo.com>
2020-07-20 01:07:59 +0300

    Menu for switching between scenarios added. Corridor scenario created.

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SwitchScene : MonoBehaviour
+{
+    public void BottleneckScene() {
+        SceneManager.LoadScene("Scenes/BottleneckScene");
+    }
+
+    public void ChickenTestScene() {
+        SceneManager.LoadScene("Scenes/ChickenTestScene");
+    }
+
+    public void CorridorScene() {
+        SceneManager.LoadScene("Scenes/CorridorScene");
+    }
+
+    public void CornerScene() {
+        SceneManager.LoadScene("Scenes/CornerScene");
+    }
+
+    public void MainScene() {
+        SceneManager.LoadScene("Scenes/MainScene");
+    }
+}
